55c9ebeba9c5f5e3611af46cead7e7c5ee848f1f
ShelpyakovAS <Shelpchel@gmail.com>
2022-11-17 19:30:28 +0500

    setup? fixture? body?

--- test_search_pdf.py
+++ test_search_pdf.py
@@ -1,8 +1,17 @@
 from selenium.webdriver.common.by import By
+import pytest
+
+'''browser.get('https://www.cbr.ru/')
+url_links = []
+for locator in ['#menu_content_Activity a', '#menu_content_FinancialMarkets a', '#menu_content_Documents a',
+                '#menu_content_AboutBank a', '#menu_content_Services a']:
+for link in browser.find_elements(By.CSS_SELECTOR, locator):
+        url_links.append(link.get_attribute('href'))'''
+url_links = []
 
 
-def test_search_pdf_on_main_page(browser):
-    link = 'https://www.cbr.ru/'
+@pytest.mark.parametrize('link', url_links)
+def test_search_pdf_on_main_page(browser, link):
     browser.get(link)
     links_have_href = browser.find_elements(By.CSS_SELECTOR, 'a[href]')
     links_pdf = []
@@ -13,6 +22,8 @@ def test_search_pdf_on_main_page(browser):
             print(href)
 
 
-
-
-
+# #menu_content_Activity a
+# #menu_content_FinancialMarkets a
+# #menu_content_Documents a
+# #menu_content_AboutBank a
+# #menu_content_Services a
