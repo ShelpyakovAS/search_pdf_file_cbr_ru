5c277ad2abd37a9c896e7069e707b22157337621
ShelpyakovAS <Shelpchel@gmail.com>
2022-11-18 12:23:07 +0500

    conftest.py     new def search_url_links(): создает список ссылок для тестов.
    test_search_pdf.py
        re @pytest.mark.parametrize('link', search_url_links())
           def test_search_pdf_on_page(browser, link):
           получает список в параметрайз и запускает поиск пдф файлов для каждой страницы в отдельном тесте

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import time
 
 
 def pytest_addoption(parser):
@@ -21,3 +22,18 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+def search_url_links():
+    browser = webdriver.Chrome()
+    browser.get('https://www.cbr.ru/')
+    time.sleep(2)
+    url_links = []
+    button = browser.find_element(By.XPATH, '/html/body/header/div[5]/div/div/div[1]/div/div[1]/div/div')
+    button.click()
+    time.sleep(2)
+    for locator in ['#menu_content_Activity a', '#menu_content_FinancialMarkets a', '#menu_content_Documents a',
+                    '#menu_content_AboutBank a', '#menu_content_Services a']:
+        for link in browser.find_elements(By.CSS_SELECTOR, locator):
+            url_links.append(link.get_attribute('href'))
+    return url_links
--- test_search_pdf.py
+++ test_search_pdf.py
@@ -1,17 +1,10 @@
 from selenium.webdriver.common.by import By
 import pytest
-
-'''browser.get('https://www.cbr.ru/')
-url_links = []
-for locator in ['#menu_content_Activity a', '#menu_content_FinancialMarkets a', '#menu_content_Documents a',
-                '#menu_content_AboutBank a', '#menu_content_Services a']:
-for link in browser.find_elements(By.CSS_SELECTOR, locator):
-        url_links.append(link.get_attribute('href'))'''
-url_links = []
+from conftest import search_url_links
 
 
-@pytest.mark.parametrize('link', url_links)
-def test_search_pdf_on_main_page(browser, link):
+@pytest.mark.parametrize('link', search_url_links())
+def test_search_pdf_on_page(browser, link):
     browser.get(link)
     links_have_href = browser.find_elements(By.CSS_SELECTOR, 'a[href]')
     links_pdf = []
@@ -20,7 +13,6 @@ def test_search_pdf_on_main_page(browser, link):
         if '.pdf' in href:
             links_pdf.append(href)
             print(href)
-
 
 # #menu_content_Activity a
 # #menu_content_FinancialMarkets a
